3eddc42165749c747b643ecca39c9188c482bec0
Jae Park <Jaep970805@gmail.com>
2020-02-10 00:50:43 +0900

    dataframe update

--- crypto.py
+++ crypto.py
@@ -4,79 +4,26 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import random
 
-upbit = ccxt.upbit()
-markets = upbit.load_markets()
-symbols = upbit.symbols
-currencies = upbit.currencies
+exchange = ccxt.binance()
+markets = exchange.load_markets()
+symbols = exchange.symbols
+currencies = exchange.currencies
 
-# def sma(clist, n):  #simple moving average function, n = number of days
-# 	psum = 0
-# 	ohlcvlist = clist
-# 	for i in range(n):
-# 		psum+=ohlcvlist[i][3]
-# 	#	print(psum)
-# 	return (psum)/n
-
-# def ema(clist, n): #exponential moving average function, n = number of days
-# 	#print(ohlcvlist)
-# 	if(len(clist) == 1):
-# 		return clist[0]
-# 	else:
-# 		temp = clist[0]
-# 		clist = clist[1:]
-# 		weight = 2/(n+1)
-# 		return weight * temp + (1-weight) * ema(clist,n)
-
-above50 = False
-above20 = False
-
-def checkabove50(data):
-	if(data.iloc[-1]['ema50']<data.iloc[-1]['ema10']):
-		print('hi')
-		return True
-	else :
-		return False
-
-def checkabove20(data):
-	if(data.iloc[-1]['ema20']<data.iloc[-1]['ema10']):
-	    print('hi')
-	    return True
-	else :
-		return False
-
-#uptrend
-#10EMA over 20 while already above 50
-
-def checkuptrend(data, above20, above50):
-    if(checkabove50(s3)):
-        above50 = True
-        if((not above20) and checkabove20(s3)):
-            above20 = True
-            return True #BUY
-    return False
-
-def checkdowntrend(data, above20, above50):
-    if(checkabove50(s3) == False):
-        above50 = False
-        if(above20 and (not checkabove20(s3))):
-            above20 = False
-            return True #SELL
-    return False
-
-#authentication
-upbit.apiKey = ''
-upbit.secret = ''
+#authentication and login
+exchange.apiKey = ''
+exchange.secret = ''
 pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
 
-if upbit.has['fetchOHLCV']:
-    #time.sleep (upbit.rateLimit/500) # time.sleep wants seconds
-    #print(upbit.fetch_ohlcv("BTC/KRW", '1d')) #gives the last 200 candlesticks
+if exchange.has['fetchOHLCV']:
+    #time.sleep (exchange.rateLimit/500) # time.sleep wants seconds
+    #print(exchange.fetch_ohlcv("BTC/KRW", '1d')) #gives the last 200 candlesticks
     #make a list of the past sma
     smalist = pd.Series()
 
-    ohlcvlist = upbit.fetch_ohlcv("BTC/KRW", '1d')
+    ohlcvlist = exchange.fetch_ohlcv("BTC/USDT", '1d')
     #ohlcvlist.reverse() #newest first
     print(len(ohlcvlist))
 
@@ -88,16 +35,109 @@ if upbit.has['fetchOHLCV']:
 
     #print(sma10.tail(20))
 
-    s3 = pd.concat([sma10,ema10,ema20,ema50], axis = 1)
-    s3.columns = ['sma10', 'ema10', 'ema20', 'ema50']
-    print(s3)
+    dfma = pd.concat([sma10,ema10,ema20,ema50], axis = 1)
+    dfma.columns = ['sma10', 'ema10', 'ema20', 'ema50']
+    print(dfma)
+    balance = exchange.fetch_balance()
+    #print(balance)
+    dfbalance = pd.Series(balance)
+    #print(exchange.privateGetAccounts(params))
+    #print(ccxt.exchanges)
 
-    print(checkuptrend(s3))
-    print(checkdowntrend(s3))
- 
+# if exchange.has['fetchOrders']:
+#     since = exchange.milliseconds () - 8640000  # -1 day from now
+#     # alternatively, fetch from a certain starting datetime
+#     # since = exchange.parse8601('2018-01-01T00:00:00Z')
+#     all_orders = []
+#     while since < exchange.milliseconds ():
+#         symbol = "ICX/BTC"  # change for your symbol
+#         limit = 10000000  # change for your limit
+#         orders = exchange.fetch_orders(symbol, since, limit)
+#         if len(orders): 
+#             since = orders[len(orders) - 1]['timestamp']
+#             all_orders += orders
+#         else:
+#             break
+#     print(len(all_orders))
 
-    #downtrend
-    #10EMA below 20 while already below 50
+print("//////////////////////////////////////")
+
+coinsowned = []
+coinsownedamount = []
+coinsinbtc = []
+coinsinusd = []
+percentagechange = []
+rate = exchange.fetch_ticker('BTC/USDT').get("bid")
+for items in dfbalance.items(): #print the coins that i own
+    if isinstance(items[1].get("total"), float):
+        if (items[1].get("total") > 0):
+            coinsowned.append(items[0])
+            coinsownedamount.append(items[1].get("total"))
+            if(items[0] == "BTC"):
+                priceinbtc = 1
+            elif(items[0] == "SBTC" or items[0] == "BCX" or items[0] == "VTHO"):
+                priceinbtc = 0
+            else:
+                priceinbtc = (exchange.fetch_ticker(items[0] + "/BTC").get("close") + exchange.fetch_ticker(items[0] + "/BTC").get("open"))/2.0
+            coinsinbtc.append(items[1].get("total") * priceinbtc)
+            coinsinusd.append(items[1].get("total") * priceinbtc * rate)
+scoinsowned = pd.Series(coinsownedamount, coinsowned, name = 'amount')
+
+
+dfcoinsowned = pd.DataFrame()
+dfcoinsowned['balance'] = scoinsowned
+dfcoinsowned['in btc'] = coinsinbtc
+dfcoinsowned['in usd'] = coinsinusd
+print(dfcoinsowned)
+
+balanceinusd = 0
+for val in dfcoinsowned["in usd"]:
+    balanceinusd += val
+print("Your total balance in USD : $" + str(balanceinusd))
+
+
+print("//////////////////////////////////////")
+
+
+#for fetching recent trade or trade orders
+print("Your recent trades")
+tradetime = []
+tradesymbol = []
+tradeamount = []
+tradeside = []
+selltrades = pd.DataFrame
+if exchange.has['fetchMyTrades']:
+    for val in coinsowned:
+        if(val!= "BTC" and val!= "SBTC" and val != "VTHO" and val != "BCX"):
+            tradess = exchange.fetch_my_trades (symbol = val + "/BTC", since = exchange.milliseconds () - 86400000, limit = 10, params = {})
+            for trade in tradess:
+                if(trade.get("side") == "sell"):
+                    tradetime.append(trade.get("datetime"))
+                    tradesymbol.append(trade.get("symbol"))
+                    tradeamount.append(trade.get("amount"))
+                    tradeside.append(trade.get("side"))          
+        #print("At " + trade.get("datetime") + " : " + trade.get("symbol") + " : " + str(trade.get("amount")) + " " + trade.get("side") + " at " + str(trade.get("price")))
+dftrades = pd.DataFrame()
+dftrades['tradetime'] = tradetime
+dftrades['tradeside'] = tradeside
+dftrades['tradesymbol'] = tradesymbol
+dftrades['tradeamount'] = tradeamount
+
+print(dftrades)
+
+print("//////////////////////////////////////")
+
+print("Your recent buys")
+isbuy = dftrades['tradeside']=="buy"
+dftradesbuy = dftrades[isbuy]
+print(dftradesbuy)
+
+print("Your recent sells")
+issell = dftrades['tradeside']=="sell"
+dftradessell = dftrades[issell]
+print(dftradessell)
+
+print("//////////////////////////////////////")
 
 
 
