0c4f4abd190459c96647cdc837f2ab0670edfb1f
JANG <jang@JANGs-MacBook-Air.local>
2020-02-17 03:30:55 -0600

    added some UI interaction in auto trade section

--- gui/auto_trade.py
+++ gui/auto_trade.py
@@ -2,35 +2,6 @@ from gui.pages import *
 
 
 class AutoTradePage(PageWidget):
-    class InputSection(QWidget):
-        def __init__(self):
-            super().__init__()
-            layout = QVBoxLayout()
-            self.setLayout(layout)
-            risk_lb = QLabel("risk")
-            risk_txt = QTextEdit("risk")
-            option2_lb = QLabel("option2")
-            option2_txt = QTextEdit("option2")
-            option3_lb = QLabel("option3")
-            option3_txt = QTextEdit("option2")
-            option4_lb = QLabel("option4")
-            option4_check = QCheckBox()
-
-            layout.addWidget(risk_lb)
-            layout.addWidget(risk_txt)
-            layout.addWidget(option2_lb)
-            layout.addWidget(option2_txt)
-            layout.addWidget(option3_lb)
-            layout.addWidget(option3_txt)
-            layout.addWidget(option4_lb)
-            layout.addWidget(option4_check)
-
-        def get_inputs(self):
-            inputs = dict()
-            for child in self.children():
-                print(child)
-            return inputs
-
     def __init__(self):
         self.drop_down_menu = None
         self.session = None
@@ -40,31 +11,35 @@ class AutoTradePage(PageWidget):
         self.layout().addWidget(self.set_panel3())
 
     def set_panel1(self):
+        def display_option(menu_index, stack_widget:QStackedWidget):
+            stack_widget.setCurrentIndex(menu_index)
+
         panel = QWidget()
         panel.setFixedSize(200, 400)
         panel.setLayout(QVBoxLayout())
+        input_section = QStackedWidget()
 
         # drop down menu for algorithm to use
         algo_menu = QComboBox()
         algo_menu.addItem('ema/sma')
         algo_menu.addItem('granger causality')
         algo_menu.addItem('other algorithm')
+        algo_menu.currentIndexChanged.connect(lambda: display_option(algo_menu.currentIndex(), input_section))
 
         # stack widgets to show unique input page for each algorithm
-        input_section = QStackedWidget()
         input_section.setFixedSize(200, 200)
 
-        j = self.InputSection()
-        i = self.InputSection()
+        ema_option = EmaOption()
+        i = OptionSection()
+        input_section.addWidget(ema_option)
         input_section.addWidget(i)
-        input_section.addWidget(j)
         input_section.setCurrentIndex(0)
 
 
         # trade button
         trade_btn = QPushButton("trade")
         trade_btn.setFixedSize(150, 30)
-        trade_btn.clicked.connect(lambda x: i.get_inputs())
+        trade_btn.clicked.connect(lambda x: print(ema_option.get_parameters()))
 
         panel.layout().addWidget(algo_menu)
         panel.layout().addWidget(input_section)
@@ -98,4 +73,38 @@ class AutoTradePage(PageWidget):
 
         panel.layout().addWidget(session_lb)
         return panel
+
+
+class OptionSection(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+    def get_parameters(self):
+        parameters = list()
+        children = [child for child in self.children() if type(child) == QTextEdit or type(child) == QCheckBox]
+        for child in children:
+            if type(child) == QTextEdit:
+                param = child.toPlainText()
+                parameters.append(param)
+        return parameters
+
+class EmaOption(OptionSection):
+    def __init__(self):
+        super().__init__()
+        # ema period
+        ema_day_lb = QLabel("ema day")
+        ema_day_value = QTextEdit()
+        ema_day_value.setFixedHeight(30)
+        # risk high mid low
+        risk_lb = QLabel("risk")
+        risk_value = QTextEdit()
+        risk_value.setFixedHeight(30)
+
+        #layout
+        self.layout().addWidget(ema_day_lb, 0, 0)
+        self.layout().addWidget(risk_lb, 1, 0)
+        self.layout().addWidget(ema_day_value, 0, 1)
+        self.layout().addWidget(risk_value, 1, 1)
 
