255c3d4064b04e1302cba9879ad6e38f643d1d6d
JANG <jang@JANGs-MacBook-Air.local>
2020-02-24 00:14:33 -0600

    added function to convert df to QTableWidget
    sizing of column further needed to be done.
    Still deciding adding functionality in the method or
    individually handled in each usage.

--- gui/pages.py
+++ gui/pages.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from pyqtgraph import *
+import pandas as pd
+from typing import List, Dict
 
 class PageWidget(QWidget):
     def __init__(self):
@@ -11,3 +13,21 @@ class PageWidget(QWidget):
         self.layout().setSpacing(0)
 
 
+def df_to_table(df: pd.DataFrame):
+    column_names: List = df.columns.to_list()
+    table_row = df.shape[0]
+    table_col = df.shape[1]
+    if df.shape[0] >= 10:
+        table_row = 10
+    table_size = (table_row, table_col)
+    print(table_size)
+    table = QTableWidget(table_size[0], table_size[1])
+    table.setFont(QFont("Times", 12))
+    table.setHorizontalHeaderLabels(column_names)
+    for col in range(table_size[1]):
+        series = df[column_names[col]]
+        for row in range(table_size[0]):
+            value = QTableWidgetItem()
+            table.setItem(row, col, value)
+            value.setText(str(series[row]))
+    return table
