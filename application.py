d8366332a68487639141fb367d0854893b036aef
JANG <yjang43@wisc.edu>
2020-03-16 00:01:14 -0500

    reorganize files again

--- application.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import *
-import sys
-import gui.main_window as run
-
-app = QApplication(sys.argv)
-main_window = run.MainWindow()
-main_window.show()
-
-app.exec_()
--- gui/data_visualization.py
+++
@@ -1,6 +0,0 @@
-from gui.pages import *
-
-class DataVisualizationPage(PageWidget):
-    def __init__(self):
-        super().__init__()
-        self.layout().addWidget(QLabel("to be announced"))
