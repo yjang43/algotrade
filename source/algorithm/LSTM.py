c256abf2556bf38bd3583ab429de07fe59bdb759
Jae Park <Jaep970805@gmail.com>
2020-03-19 01:23:35 +0900

    crypto class file

--- source/algorithm/LSTM.py
+++ source/algorithm/LSTM.py
@@ -30,6 +30,72 @@ def create_lookback(dataset, look_back=1):
         Y.append(dataset[i + look_back, 0])
     return np.array(X), np.array(Y)
 
+# This function prepares random train/test split,
+# scales data with MinMaxScaler, create time series labels (Y)
+def get_split(working_data, n_train, n_test, look_back = 1):
+    # get a point from which we start to take train dataset and after it - test dataset
+    start_point = randint(0, (len(working_data)-n_test-n_train))
+    df_train = working_data[start_point:start_point+n_train]
+    df_test = working_data[start_point+n_train:start_point+n_train+n_test]
+
+    training_set = df_train.values
+    training_set = np.reshape(training_set, (len(training_set), 1))
+    test_set = df_test.values
+    test_set = np.reshape(test_set, (len(test_set), 1))
+
+    # scale datasets
+    scaler_cv = MinMaxScaler()
+    training_set = scaler_cv.fit_transform(training_set)
+    test_set = scaler_cv.transform(test_set)
+
+    # create datasets which are suitable for time series forecasting
+    X_train, Y_train = create_lookback(training_set, look_back)
+    X_test, Y_test = create_lookback(test_set, look_back)
+
+    # reshape datasets so that they will be ok for the requirements of the models in Keras
+    X_train = np.reshape(X_train, (len(X_train), 1, X_train.shape[1]))
+    X_test = np.reshape(X_test, (len(X_test), 1, X_test.shape[1]))
+
+    return X_train, Y_train, X_test, Y_test, scaler_cv, start_point
+
+# This function takes datasets from the previous function as input and train model using these datasets
+def train_model(X_train, Y_train, X_test, Y_test):
+    # initialize sequential model, add bidirectional LSTM layer and densely connected output neuron
+    model = Sequential()
+    model.add(GRU(256, input_shape=(X_train.shape[1], X_train.shape[2])))
+    model.add(Dense(1))
+
+    # compile and fit the model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(X_train, Y_train, epochs = 100, batch_size = 16, shuffle = False,
+                    validation_data=(X_test, Y_test), verbose=0,
+                    callbacks = [EarlyStopping(monitor='val_loss',min_delta=5e-5,patience=20,verbose=0)])
+    return model
+
+# This function uses trained model and test dataset to calculate RMSE
+def get_rmse(model, X_test, Y_test, scaler, start_point, working_data, n_train):
+    # add one additional data point to align shapes of the predictions and true labels
+    X_test = np.append(X_test, scaler.transform(working_data.iloc[start_point+n_train+len(X_test)][0]))
+    X_test = np.reshape(X_test, (len(X_test), 1, 1))
+
+    # get predictions and then make some transformations to be able to calculate RMSE properly in USD
+    prediction = model.predict(X_test)
+    prediction_inverse = scaler.inverse_transform(prediction.reshape(-1, 1))
+    Y_test_inverse = scaler.inverse_transform(Y_test.reshape(-1, 1))
+    prediction2_inverse = np.array(prediction_inverse[:,0][1:])
+    Y_test2_inverse = np.array(Y_test_inverse[:,0])
+
+    #calculate RMSE
+    RMSE = sqrt(mean_squared_error(Y_test2_inverse, prediction2_inverse))
+    return RMSE, prediction2_inverse
+
+
+def workflow(working_data, get_split, train_model, get_rmse,n_train = 250,n_test = 50,look_back = 1):
+    X_train, Y_train, X_test, Y_test, scaler, start_point = get_split(working_data, n_train, n_test)
+    model = train_model(X_train, Y_train, X_test, Y_test)
+    RMSE, predictions = get_rmse(model, X_test, Y_test, scaler, start_point, working_data, n_train)
+    return RMSE, predictions
+
 
 
 exchange = ccxt.binance()
@@ -39,6 +105,7 @@ mydf.isnull().values.any() #check for presence of null values
 myseries = pd.concat([mydf['Time'], mydf['Close']], axis=1)
 myseries = myseries.set_index('Time')
 print(myseries)
+print(len(myseries))
 
 d0 = date(2018, 10, 29)
 d1 = date(2020, 1, 1)
@@ -131,7 +198,8 @@ trace6 = plt.scatter(x = np.arange(0, len(history.history['val_loss']), 1), y = 
 data = [trace5, trace6]
 plt.show()
 
-X_test = np.append(X_test, scaler.transform(working_data.iloc[-1][0]))
+# print("data : ", scaler.transform(working_data.iloc[-1][0]))
+X_test = np.append(X_test, X_test[len(X_test)-1])
 X_test = np.reshape(X_test, (len(X_test), 1, 1))
 
 # get predictions and then make some transformations to be able to calculate RMSE properly in USD
@@ -141,8 +209,41 @@ Y_test_inverse = scaler.inverse_transform(Y_test.reshape(-1, 1))
 prediction2_inverse = np.array(prediction_inverse[:,0][1:])
 Y_test2_inverse = np.array(Y_test_inverse[:,0])
 
-trace1 = plt.scatter(x = np.arange(0, len(prediction2_inverse), 1), y = prediction2_inverse, label = 'Predicted labels', color = 'r')
-trace2 = plt.scatter(x = np.arange(0, len(Y_test2_inverse), 1), y = Y_test2_inverse, label = 'True labels', color = 'b')
+plt.plot(np.arange(0, len(prediction2_inverse), 1), prediction2_inverse, label = 'Predicted labels', color = 'r')
+plt.plot(np.arange(0, len(Y_test2_inverse), 1), Y_test2_inverse, label = 'True labels', color = 'b')
+plt.title("Comparison graph")
+plt.xlabel("Day number")
+plt.ylabel("Price, USD")
 plt.show()  
+
+RMSE = sqrt(mean_squared_error(Y_test2_inverse, prediction2_inverse))
+print('Test RMSE: %.3f' % RMSE)
+
+Test_Dates = myseries[len(myseries)-days_from_train + 43:].index
+
+fig, ax = plt.subplots()
+
+plt.plot(Test_Dates, Y_test2_inverse, label = 'Actual price', color = 'r')
+plt.plot(Test_Dates, prediction2_inverse, label = 'Predicted price', color = 'b')
+
+every_nth = 20  
+for n, label in enumerate(ax.xaxis.get_ticklabels()):
+    if n % every_nth != 0:
+        label.set_visible(False)
+
+plt.title("Comparison graph")
+plt.xlabel("Dates")
+plt.ylabel("Price, USD")
+plt.show()  
+
+
+RMSE, predictions = workflow(working_data, get_split, train_model, get_rmse, n_train = 200,n_test = 20)
+print('Test GRU model RMSE: %.3f' % RMSE)
+
+
+
+
+
+
 
 
