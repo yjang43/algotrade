88c6201c28573cf743d5ef31db062e1190614d33
Jaep0805 <53741483+Jaep0805@users.noreply.github.com>
2020-07-01 13:50:04 +0900

    mid update

---
+++ source/backend/main.py
@@ -0,0 +1,15 @@
+import ccxt
+import random
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import random
+import mainfunctions
+from threading import Timer
+
+
+
+#mainfunctions.ohlcvsave("BTC/USDT");
+#mainfunctions.getBalance();
+
