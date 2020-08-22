f0267f28470f9586421a5aefd515c5a49d61e437
Jaep0805 <53741483+Jaep0805@users.noreply.github.com>
2020-08-22 22:04:38 +0900

    ,

--- beta/tmp.py
+++ beta/tmp.py
@@ -4,24 +4,6 @@
 import queue
 
 order_queue = queue.Queue()
-order0 = {
-    'session_id': 'demo_session',
-    'order_info': {
-        'symbol': 'VET/USDT',
-        'side': 'sell',
-        'amount': 1000
-    }
-}
-order1 = {
-    'session_id': 'demo_session',
-    'order_info': {
-        'symbol': 'NULS/USDT',
-        'side': 'sell',
-        'amount': 1000
-    }
-}
-order_queue.put(order0)
-order_queue.put(order1)
 
 
 """structure
